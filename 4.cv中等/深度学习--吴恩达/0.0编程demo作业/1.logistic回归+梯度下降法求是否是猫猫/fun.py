501a6f3f672f42e8c99b832c343faa55c8cb1b5d
tj-messi <2715978522@qq.com>
2024-09-01 18:47:00 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/fun.py
+++ 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/fun.py
@@ -2,10 +2,14 @@ import math
 import numpy as np
 
 def basic_sigmoid(x):
+    #基础basic函数
     return 1 / (1 + np.exp(-x))
 
-x = np.array([1, 2, 3])
-print(basic_sigmoid(x))
-
 def sigmoid_derivative(x):
-    
+    #sigmoid函数函数
+    s = basic_sigmoid(x)
+    ds = s * (1 - s)
+    return ds
+
+
+
