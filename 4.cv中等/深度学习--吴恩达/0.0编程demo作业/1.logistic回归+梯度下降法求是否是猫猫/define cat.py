029484655dd751b019b8e7e407dfb52c7a41e27d
tj-messi <2715978522@qq.com>
2024-09-01 21:19:26 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/define cat.py
+++ 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/define cat.py
@@ -9,5 +9,19 @@ from lr_utils import load_dataset
 # 加载数据集
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
-# 预处理图片
+# 预处理图片的几个数据：训练数量、测试数量、图片高度和宽度
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+# 转换numpy数组为展开的(num_px * num_px * 3, 1)的矩阵
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+
+# 归一化数据
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+
 
