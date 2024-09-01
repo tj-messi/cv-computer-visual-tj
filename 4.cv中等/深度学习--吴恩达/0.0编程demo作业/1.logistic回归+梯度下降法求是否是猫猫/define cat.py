08e76640e441b030e006b044c44a45c0cb16d681
tj-messi <2715978522@qq.com>
2024-09-01 23:39:25 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/define cat.py
+++ 4.cv中等/深度学习--吴恩达/0.0编程demo作业/1.logistic回归+梯度下降法求是否是猫猫/define cat.py
@@ -26,39 +26,16 @@ test_set_x = test_set_x_flatten/255.
 
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
-    """
-    Builds the logistic regression model by calling the function you've implemented previously
-
-    Arguments:
-    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
-    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
-    print_cost -- Set to true to print the cost every 100 iterations
-
-    Returns:
-    d -- dictionary containing information about the model.
-    """
-
-    ### START CODE HERE ###
-
     # initialize parameters with zeros (≈ 1 line of code)
     w, b = fun.initialize_with_zeros(X_train.shape[0])
-
     # Gradient descent (≈ 1 line of code)
     parameters, grads, costs = fun.optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
-
     # Retrieve parameters w and b from dictionary "parameters"
     w = parameters["w"]
     b = parameters["b"]
-
     # Predict test/train set examples (≈ 2 lines of code)
     Y_prediction_test = fun.predict(w, b, X_test)
     Y_prediction_train = fun.predict(w, b, X_train)
-
-    ### END CODE HERE ###
 
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
@@ -75,3 +52,16 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     return d
 
 d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+## START CODE HERE ## (PUT YOUR IMAGE NAME)
+my_image = "cat1.jpg"   # change this to the name of your image file
+## END CODE HERE ##
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = fun.predict(d["w"], d["b"], my_image)
+
+plt.imshow(image)
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
