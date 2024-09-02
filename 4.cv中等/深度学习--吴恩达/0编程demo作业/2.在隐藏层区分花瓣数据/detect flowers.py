c2d0e4441c46fa262c0391c9a310620d5bcf8738
tj-messi <2715978522@qq.com>
2024-09-02 21:58:34 +0800

    1

---
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/detect flowers.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+np.random.seed(1) # 设置随机种子
+
