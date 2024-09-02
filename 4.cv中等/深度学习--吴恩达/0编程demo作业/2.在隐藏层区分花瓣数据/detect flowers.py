8598383a964405f24e0e47cca610368ddd9673ff
tj-messi <2715978522@qq.com>
2024-09-02 23:02:12 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/detect flowers.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/detect flowers.py
@@ -10,8 +10,18 @@ np.random.seed(1) # 设置指定的随机种子，保证每次运行结果一致
 X, Y = load_planar_dataset() # 加载数据集
 
 '''
-#Visualize the data:
+#可视化数据集:
 plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
 plt.show()
+'''
+
+shape_X = X.shape
+shape_Y = Y.shape
+m = Y.shape[1]
+
+'''
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
 '''
 
