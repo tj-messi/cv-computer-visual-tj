e7d818be9a087eedb0831b4686bbed2bf85a8188
tj-messi <2715978522@qq.com>
2024-09-03 10:46:06 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/fun.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/fun.py
@@ -27,3 +27,48 @@ print("The size of the output layer is: n_y = " + str(n_y))
 '''
 
 
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+
+    np.random.seed(2)  # we set up a seed so that your output matches ours although the initialization is random.
+
+    ### START CODE HERE ### (≈ 4 lines of code)
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    ### END CODE HERE ###
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+'''
+n_x, n_h, n_y = testCases.initialize_parameters_test_case()
+
+parameters = initialize_parameters(n_x, n_h, n_y)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+'''
+
