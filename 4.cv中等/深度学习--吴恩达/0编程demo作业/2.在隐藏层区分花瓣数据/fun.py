0cfb875cc989a8308468e63d8fe311dc0a766f7c
tj-messi <2715978522@qq.com>
2024-09-03 10:52:31 +0800

    Update fun.py

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/fun.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/fun.py
@@ -17,7 +17,6 @@ def layer_sizes(X, Y):
     n_y = Y.shape[0]  # size of output layer
     return (n_x, n_h, n_y)
 
-
 '''
 X_assess, Y_assess = testCases.layer_sizes_test_case()
 (n_x, n_h, n_y) = layer_sizes(X_assess, Y_assess)
@@ -25,8 +24,6 @@ print("The size of the input layer is: n_x = " + str(n_x))
 print("The size of the hidden layer is: n_h = " + str(n_h))
 print("The size of the output layer is: n_y = " + str(n_y))
 '''
-
-
 def initialize_parameters(n_x, n_h, n_y):
     """
     Argument:
@@ -72,3 +69,47 @@ print("W2 = " + str(parameters["W2"]))
 print("b2 = " + str(parameters["b2"]))
 '''
 
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+def forward_propagation(X, parameters):
+    """
+        Argument:
+        X -- input data of size (n_x, m)
+        parameters -- python dictionary containing your parameters (output of initialization function)
+
+        Returns:
+        A2 -- The sigmoid output of the second activation
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+        """
+    # 从parameters中取出W1, b1, W2, b2
+    W1 = parameters.get('W1')
+    b1 = parameters.get('b1')
+    W2 = parameters.get('W2')
+    b2 = parameters.get('b2')
+    A0 = X
+
+    # 前向传播计算
+    Z1 = np.dot(W1, A0) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+
+    #设定A2为列向量输出
+    assert (A2.shape == (1, X.shape[1]))
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+
+    return A2, cache
+
+'''
+X_assess, parameters = testCases.forward_propagation_test_case()
+
+A2, cache = forward_propagation(X_assess, parameters)
+
+# Note: we use the mean here just to make sure that your output matches ours.
+print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
+'''
