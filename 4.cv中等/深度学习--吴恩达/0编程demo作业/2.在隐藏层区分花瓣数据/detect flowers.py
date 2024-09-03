f7c72d630612a31dcb859dd17a20c70db8afb3dd
tj-messi <2715978522@qq.com>
2024-09-03 09:57:41 +0800

    Update detect flowers.py

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/detect flowers.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/2.在隐藏层区分花瓣数据/detect flowers.py
@@ -20,8 +20,25 @@ shape_Y = Y.shape
 m = Y.shape[1]
 
 '''
+#验证x和y的shape是否正确
 print ('The shape of X is: ' + str(shape_X))
 print ('The shape of Y is: ' + str(shape_Y))
 print ('I have m = %d training examples!' % (m))
 '''
 
+'''
+# 可以先使用sklrean的LogisticRegressionCV来训练模型，然后画出决策边界，实现一个简单的二分类
+# 注意Y的维度不对，需要用ravel()函数将其变成一维数组
+clf = sklearn.linear_model.LogisticRegressionCV()
+clf.fit(X.T, np.ravel(Y.T))
+# Plot the decision boundary for logistic regression
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title("Logistic Regression")
+
+LR_predictions = clf.predict(X.T)
+accuracy = (np.dot(Y, LR_predictions) + np.dot(1 - Y, 1 - LR_predictions)) / np.sum(Y) * 100
+print('Accuracy of logistic regression: %d %%' % accuracy +
+      ' (percentage of correctly labelled datapoints)')
+
+plt.show()
+'''
