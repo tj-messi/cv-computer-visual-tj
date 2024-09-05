f503ba3025fd96ffd18bfef2a5df548d637ad6ab
tj-messi <2715978522@qq.com>
2024-09-05 23:43:11 +0800

    Update detect-cat.py

---
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/3.建设自己的深层神经网络/detect-cat.py
@@ -0,0 +1,14 @@
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn_app_utils_v2 import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
