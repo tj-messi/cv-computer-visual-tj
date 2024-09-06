47a135dcaffc594d54f6d2b5daf9b33d4e28b0ad
tj-messi <2715978522@qq.com>
2024-09-06 10:12:18 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/3.建设自己的深层神经网络/detect-cat.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/3.建设自己的深层神经网络/detect-cat.py
@@ -12,3 +12,40 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 np.random.seed(1)
+
+# 加载数据集
+train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+'''
+# Example of a picture
+index = 7
+plt.imshow(train_x_orig[index])
+print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+plt.show()
+'''
+
+# 取出数据集的大小
+m_train = train_x_orig.shape[0]
+m_test = test_x_orig.shape[0]
+num_px = train_x_orig.shape[1]
+
+'''
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(train_x_orig.shape))
+print ("train_y shape: " + str(train_y.shape))
+print ("test_x_orig shape: " + str(test_x_orig.shape))
+print ("test_y shape: " + str(test_y.shape))
+'''
+
+# 统一重新调整图片大小
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+# 标准化数据在0到1之间
+train_x = train_x_flatten/255.
+test_x = test_x_flatten/255.
+
+print ("train_x's shape: " + str(train_x.shape))
+print ("test_x's shape: " + str(test_x.shape))
