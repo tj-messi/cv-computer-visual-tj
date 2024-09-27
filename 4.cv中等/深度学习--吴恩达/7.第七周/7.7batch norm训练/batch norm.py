0feea0c22e3f05a48796c154a31fbedb4c724f34
tj-messi <2715978522@qq.com>
2024-09-27 16:26:17 +0800

    1

---
+++ 4.cv中等/深度学习--吴恩达/7.第七周/7.7batch norm训练/batch norm.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+bs = 64
+
+print("Pytorch Batch Norm Layer详解")
+print("--- 2D input:(mini_batch, num_feature) ---")
+# With Learnable Parameters
+m = nn.BatchNorm1d(400)  # 例如，房价预测：x的特征数是400，y是房价
+# Without Learnable Parameters（无学习参数γ和β）
+# m = nn.BatchNorm1d(100, affine=False)
+inputs = torch.randn(bs, 400)
+print(m(inputs).shape)
+
+
+print("Batch Norm层的γ和β是要训练学习的参数")
+print("γ:", m.state_dict()['weight'].shape)  # gammar
+print("β:", m.state_dict()['bias'].shape)  # beta
+print("")
+
+
+print("--- 3D input:(mini_batch, num_feature, other_channel) ---")
+m = nn.BatchNorm1d(32)
+inputs = torch.randn(bs, 32, 32)  # 这种格式的数据不常用
+print(m(inputs).shape)
+
+print("Batch Norm层的γ和β是要训练学习的参数")
+print("γ:", m.state_dict()['weight'].shape)  # gammar
+print("β:", m.state_dict()['bias'].shape)  # beta
+print("")
+
+
+print("--- 4D input:(mini_batch, num_feature, H, W) ---")
+m = nn.BatchNorm2d(3)  # 例如, CIFAR10数据集是三通道的，3x32x32
+
+inputs = torch.randn(bs, 3, 32, 32)
+m.train(inputs)
+print(m(inputs).shape)
+
+print("Batch Norm层的γ和β是要训练学习的参数")
+print("γ:", m.state_dict()['weight'].shape)  # gammar
+print("β:", m.state_dict()['bias'].shape)  # beta
+print("Batch Norm层的running_mean和running_var是统计量（主要用于预测阶段）")
+print("running_mean:", m.state_dict()['running_mean'].shape)
+print("running_var:", m.state_dict()['running_var'].shape)
+
