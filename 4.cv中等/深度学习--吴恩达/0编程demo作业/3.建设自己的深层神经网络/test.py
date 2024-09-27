00759e6319334eaff5e3bb8ca5c52cd692a291c1
tj-messi <2715978522@qq.com>
2024-09-27 15:23:55 +0800

    1

--- 4.cv中等/深度学习--吴恩达/0编程demo作业/3.建设自己的深层神经网络/test.py
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/3.建设自己的深层神经网络/test.py
@@ -1,10 +1,12 @@
 import torch
 
 # 检查CUDA是否可用
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 创建张量并移动到GPU上
-x = torch.randn(3, 3).to(device)
+x = torch.randn(3, 3)
+print(x)
+x=x*x.T
 print(x)
 
 # 创建模型并移动到GPU上
