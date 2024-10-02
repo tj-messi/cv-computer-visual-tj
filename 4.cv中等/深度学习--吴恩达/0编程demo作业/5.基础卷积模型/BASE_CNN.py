83235c895550d2c30b2208ea66d01f1b2be0f164
tj-messi <2715978522@qq.com>
2024-10-02 20:06:42 +0800

    1

---
+++ 4.cv中等/深度学习--吴恩达/0编程demo作业/5.基础卷积模型/BASE_CNN.py
@@ -0,0 +1,13 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+#%load_ext autoreload
+#%autoreload 2
+
+np.random.seed(1)
