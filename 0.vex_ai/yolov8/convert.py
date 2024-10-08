2adc1bc5248ddad67e42ba176151d611a71255e4
tj-messi <2715978522@qq.com>
2024-10-08 22:41:12 +0800

    1

--- 0.vex_ai/yolov8/convert.py
+++ 0.vex_ai/yolov8/convert.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 
 # 确保你已经加载了模型
 # 假设你的模型类是 MyModel，并且已经初始化过
-model = YOLO(model="best.pt")  # 请替换为你的模型初始化代码
+model = YOLO(model="0.vex_ai/yolov8/best.pt")  # 请替换为你的模型初始化代码
 
-model.export(format="onnx")  # 导出为 ONNX
+model.export(format="onnx",imgsz=320)  # 导出为 ONNX
