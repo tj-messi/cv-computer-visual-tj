a29a9c5fa72fdf882e724fcabe24830166c20ea1
tj-messi <2715978522@qq.com>
2024-10-09 08:21:57 +0800

    1

--- 4.cv中等/计算机视觉--斯坦福/编程作业/practice1/KNN.py
+++ 4.cv中等/计算机视觉--斯坦福/编程作业/practice1/KNN.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from data_utils import load_CIFAR10
+from cs231n.data_utils import load_CIFAR10
 import matplotlib.pyplot as plt
 
 plt.rcParams['figure.figsize']=(10.0,8.0)#修改图片大小
@@ -8,6 +8,14 @@ plt.rcParams['image.interpolation']='nearest'#修改插值方式
 plt.rcParams['image.cmap']='gray'#使用灰度图
 
 cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+
+try:
+   del X_train, y_train
+   del X_test, y_test
+   print('Clear previously loaded data.')
+except:
+   pass
+
 X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
 print('Training data shape: ', X_train.shape)
--- 4.cv中等/计算机视觉--斯坦福/编程作业/practice1/cs231n/data_utils.py
+++ 4.cv中等/计算机视觉--斯坦福/编程作业/practice1/cs231n/data_utils.py
@@ -33,14 +33,14 @@ def load_CIFAR10(ROOT):
     xs = []
     ys = []
     for b in range(1, 6):
-        f = os.path.join(ROOT, "data_batch_%d" % (b,))
+        f = os.path.join(ROOT+'/', "data_batch_%d" % (b,))
         X, Y = load_CIFAR_batch(f)
         xs.append(X)
         ys.append(Y)
     Xtr = np.concatenate(xs)
     Ytr = np.concatenate(ys)
     del X, Y
-    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, "test_batch"))
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT+'/', "test_batch"))
     return Xtr, Ytr, Xte, Yte
 
 
