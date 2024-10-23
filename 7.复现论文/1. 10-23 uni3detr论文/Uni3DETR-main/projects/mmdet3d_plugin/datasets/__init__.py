88f26e57c3930fe224bb38e0e4d6a9f164319aff
tj-messi <2715978522@qq.com>
2024-10-23 19:11:22 +0800

    1

---
+++ 7.复现论文/1. 10-23 uni3detr论文/Uni3DETR-main/projects/mmdet3d_plugin/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .nuscenes_dataset import NuScenesSweepDataset
+from .sunrgbd_dataset_ov import SUNRGBDDataset_OV
+
+__all__ = [
+    'NuScenesSweepDataset', 'SUNRGBDDataset_OV'
+]
---
+++ 7.复现论文/1. 10-23 uni3detr论文/Uni3DETR-main/projects/mmdet3d_plugin/models/dense_heads/__init__.py
@@ -0,0 +1,4 @@
+from .uni3detr_head import Uni3DETRHead
+from .uni3detr_head_clip import Uni3DETRHeadCLIP
+
+__all__ = ['Uni3DETRHead', 'Uni3DETRHeadCLIP']
