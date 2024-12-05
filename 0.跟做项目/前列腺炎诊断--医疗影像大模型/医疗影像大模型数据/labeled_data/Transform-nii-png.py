0525b2630bf6077648fabb53ce41125adb77b071
tj-messi <2715978522@qq.com>
2024-12-05 23:12:58 +0800

    1

--- 0.跟做项目/前列腺炎诊断--医疗影像大模型/医疗影像大模型数据/labeled_data/Transform-nii-png.py
+++ 0.跟做项目/前列腺炎诊断--医疗影像大模型/医疗影像大模型数据/labeled_data/Transform-nii-png.py
@@ -4,8 +4,8 @@ import imageio
 import os
 
 
-nii_file = 'fortrain/us_images/case000000.nii'
-save_dir = 'train_png/images'
+nii_file = 'case000000.nii'
+save_dir = 'train_png/label'
 img = nib.load(nii_file)
 
 img_fdata = img.get_fdata() # 获取图像数据
@@ -13,9 +13,10 @@ img_fdata = img.get_fdata() # 获取图像数据
 # 选择一个切片进行可视化
 slice_index = 0
 slice_data = img_fdata[45, :, :]
-
+slice_data = slice_data[:,:,0]
 plt.imshow(slice_data, cmap='gray')
 plt.axis('off')
 #plt.show()
 
 plt.savefig(os.path.join(save_dir, f'slice_{slice_index}.png'), bbox_inches='tight', pad_inches=0)
+
