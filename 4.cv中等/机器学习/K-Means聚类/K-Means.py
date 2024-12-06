a261b37672dcbade70149f277edf2d2dd3398824
tj-messi <2715978522@qq.com>
2024-12-07 02:59:53 +0800

    1

--- 4.cv中等/机器学习/K-Means聚类/K-Means.py
+++ 4.cv中等/机器学习/K-Means聚类/K-Means.py
@@ -4,191 +4,173 @@ import numpy as np
 from math import sqrt
 import random
 from matplotlib import pyplot as plt
+import pandas as pd
 
-def load_dataset(filename,seperator):
-    """数据的导入"""
-    data_list=[]
-    fr=open(filename)
-    for line in fr.readlines():
-        curline=line.strip().split(seperator)
-        float_list=[float(element) for element in curline if element]
-        data_list.append(float_list)
+
+def load_dataset(filename, separator=','):
+    """Load dataset with proper handling of header and separator."""
+    data_list = []
+    try:
+        df = pd.read_csv(filename, sep=separator, header=None)
+        data_list = df.values.tolist()
+    except Exception as e:
+        print("Error loading dataset:", e)
     return data_list
 
-def show_cluster(dataset,central_points,result,save_name):
-    """可视化：散点图的显示以及保存"""
-    points_num=len(central_points)
-    data_num=len(result)
-    color = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "gray", "brown", "cyan", "magenta", "gold"]
-    for i in range(data_num):
-        data_color=color[int(result[i][0])]
-        plt.plot(dataset[i][0],dataset[i][1],marker='o',color=data_color)
-    for j in range(points_num):
-        plt.plot(central_points[j][0], central_points[j][1],marker='^',color='k')
-    plt.savefig(str(save_name))
-    plt.show()
 
-def showElbow(dataset,max_centers):
-    """计算手肘法对应的折线图"""
-    k_distance=[]
-    for k in range(max_centers):
-        sum_all=0
-        for i in range(1):      #每一个K进行50次，最后k_distance取其平均值
-            central_points, result = Kmeans(dataset, k)
-            sum_all+=sum([sqrt(result[i][1]) for i in range(len(result))])#将每一个欧氏距离误差相加
-        k_distance.append(sum_all/10)
+def calculateEucl(vector1, vector2):
+    """Calculate Euclidean distance between two points."""
+    return sqrt(sum((v1 - v2) ** 2 for v1, v2 in zip(vector1, vector2)))
+
+
+def make_central_points(dataset, k):
+    """Generate k random centroids."""
+    attributes_num = len(dataset[0])  # Number of features
+    central_points = np.zeros((k, attributes_num))
+    for index in range(attributes_num):
+        index_min = min([child[index] for child in dataset])
+        index_max = max([child[index] for child in dataset])
+        index_range = index_max - index_min
+        for child in central_points:
+            child[index] = index_min + index_range * random.random()
+    return central_points
+
+
+def Kmeans(dataset, k):
+    """K-means clustering algorithm."""
+    data_num = len(dataset)
+    attributes_num = len(dataset[0])
+    result = np.zeros((data_num, 2))  # Store cluster index and squared distance
+    central_points = make_central_points(dataset, k)
+    loop = True
+    while loop:
+        loop = False
+        for i in range(data_num):
+            min_dis, min_index = float('inf'), -1
+            for j in range(k):
+                distance = calculateEucl(dataset[i], central_points[j])
+                if distance < min_dis:
+                    min_dis, min_index = distance, j
+            result[i][1] = min_dis ** 2
+            if result[i][0] != min_index:
+                result[i][0], loop = min_index, True
+        for j in range(k):
+            cluster_points = [dataset[i] for i in range(data_num) if result[i][0] == j]
+            if cluster_points:
+                central_points[j] = np.mean(cluster_points, axis=0)
+    return central_points, result
+
+
+def showElbow(dataset, max_centers):
+    """Plot the elbow method curve."""
+    k_distance = []
+    for k in range(1, max_centers + 1):
+        central_points, result = Kmeans(dataset, k)
+        total_distance = sum(result[i][1] for i in range(len(result)))
+        k_distance.append(total_distance)
     plt.plot(range(1, max_centers + 1), k_distance, marker='o')
     plt.title('Elbow Method')
     plt.xlabel('Number of Clusters')
-    plt.ylabel('Sum of Distances')
-    plt.savefig("Elbow")
+    plt.ylabel('Sum of Squared Distances')
+    plt.savefig("Elbow_Method.png")
     plt.show()
 
-def calculateEucl(vector1,vector2):
-    """计算两点之间欧式距离"""
-    sum=0
-    for index in range(len(vector1)):
-        sum+=(vector1[index]-vector2[index])**2
-    return sqrt(sum)
-def make_central_points(dataset,k):
-    """构建一个包含k个随机质心的集合：这是k-means的不足之处，留待改进"""
-    attributes_num=len(dataset[0])                #特征数量
-    central_points=np.zeros((k,attributes_num))   #创建一个k行n列的二维数组
-    for index in range(attributes_num):
-        index_min=min([child_list[index] for child_list in dataset])
-        index_max=max([child_list[index] for child_list in dataset])
-        index_range=index_max-index_min
-        for child_array in central_points:        #每一个点都是均值点
-            child_array[index]=index_min+index_range*np.random.rand()
-    return central_points                         #返回一个k行n列的二维数组
 
-def nearset(data,centers):
-    """计算该数据点到已经初始化的数据中心的最小距离"""
-    num_centers=len(centers)
-    distance_list=[calculateEucl(data,centers[i]) for i in range(num_centers)]
-    return min(distance_list)
-
-def Kmeanspp_central_points(dataset,k):
-    """k-means++初始化中心点的目标是使得中心点的距离尽可能远"""
-    data_num=len(dataset)
-    attributes_num = len(dataset[0])
-    central_points=np.zeros((k,attributes_num))
-    random_center_index=np.random.randint(0,data_num)  #随机选择一个样本点作为中心点
-    central_points[0]=dataset[random_center_index]         #将这个随机点作为样本点
-    distance=[0.0 for i in range(data_num)]                #初始化一个距离的序列
-    for i in range(1,k):              #对于每一个中心点
-        for j in range(data_num):     #遍历所有的数据点
-            distance[j]=nearset(dataset[j],central_points[0:i])    #计算所有数据点最小距离
-        sum_all=sum(distance)*random.random()
-        for index in range(len(distance)):
-            sum_all-=distance[index]
-            if sum_all<0:
-                central_points[i]=dataset[index]
-                break
-    return central_points
-
-def Kmeans(dataset,k):
-    """K-means聚类算法"""
-    data_num=len(dataset)
-    if data_num==0:
-        return [[]],[[]]
-    attributes_num = len(dataset[0])
-    result=np.zeros((data_num,2))                 #创建一个data_num行2列的二维数组,保存每一个数据点的信息
-    central_points=Kmeanspp_central_points(dataset,k)
-    loop=True
-    while loop:
-        loop=False
-        #形成以每一个中心点为核心的类簇
-        for data_index in range(data_num):         #计算每一个数据点到中心点的距离
-            min_dis,min_index=float('inf'),-1
-            for points_index in range(k):          #求该数据点到这些中心点的最小距离
-                distance=calculateEucl(dataset[data_index],central_points[points_index])
-                if distance<min_dis:
-                    min_dis,min_index=distance,points_index
-            result[data_index][1]=min_dis**2       #修改每一次的最小距离
-            if result[data_index][0]!=min_index:   #形成类簇
-                result[data_index][0],loop=min_index,True
-        for points_index in range(k):  #遍历每一个中心点,修改中心点
-            #含有所有子元素的列表
-            child_datalist=[dataset[i] for i in range(data_num) if result[i][0]==points_index]
-            if len(child_datalist)==0:
-                continue
-            for attribute in range(attributes_num):
-                # 含有每一个子元素的相应属性的列表
-                attribute_list=[child[attribute] for child in child_datalist]
-                #修改中心点的坐标
-                central_points[points_index][attribute]=sum(attribute_list)/len(attribute_list)
-    return central_points,result
-
-def BiKmeans(dataset,k,judge):
-    """二分Kmeans算法"""
+def BiKmeans(dataset, k, judge=False):
+    """Binary K-means clustering algorithm."""
     data_num = len(dataset)
-    record=np.zeros((data_num,2))                      #保存每一个数据点的中心点以及欧氏距离
-    central_points=make_central_points(dataset,1)   #质心初始化为所有质点的平均值,[[0]]
-    central_points=central_points.tolist()
-    for j in range(data_num):                          #先计算每一个点到该中心点的距离
-        record[j][1]=calculateEucl(central_points[0],dataset[j])**2   #计算每一个数据点初始的欧氏距离
-    while len(central_points)<k:                       #在达到目标个数据点之前都做循环
+    record = np.zeros((data_num, 2))  # Store cluster index and squared distance
+    central_points = [np.mean(dataset, axis=0).tolist()]  # Start with one centroid
+    for i in range(data_num):
+        record[i][1] = calculateEucl(central_points[0], dataset[i]) ** 2  # Initial distances
+
+    while len(central_points) < k:
         if judge:
-            show_cluster(dataset, central_points, record,len(central_points))
-            print("程序仍在进行(分步可视化)，请稍等")
-        min_dis=float('inf')
-        choose_point_index=-1             #表示选择哪一个中心点进行二分
-        add_centers,data_result=[[]],[[]] #二分后的中心坐标、该簇下二分后的result
-        for i in range(len(central_points)):           #对于中心点列表中的每一个点
-            #获取第i个中心点下的所有数据点
-            child_data_list=[dataset[index] for index in range(data_num) if record[index][0]==i]
-            if len(child_data_list)==0:    #如果该中心点没有子点，则一定不进行二分
+            print(f"Current number of centroids: {len(central_points)}. Visualization step...")
+        min_sse = float('inf')
+        best_split_idx = -1
+        best_new_centers = None
+        best_new_result = None
+
+        for i in range(len(central_points)):
+            cluster_points = [dataset[j] for j in range(data_num) if record[j][0] == i]
+            if not cluster_points:
                 continue
-            temp_centers,temp_result=Kmeans(child_data_list,2)
-            split_sse=sum([temp_result[i][1] for i in range(len(temp_result))])   #计算所有点到各自中心点距离的平方和
-            splitnot_sse=sum([record[index][1] for index in range(data_num) if record[index][0]!=i])
-            if split_sse+splitnot_sse<min_dis:
-                choose_point_index,min_dis,add_centers=i,split_sse+splitnot_sse,temp_centers
-                data_result=temp_result.copy()         #对第i号簇进行的划分后的点集
-        #改变data_result的索引
-        for i in range(len(data_result)):
-            if int(data_result[i][0])==0:       #用第0号分点替换choose_point_index
-                data_result[i][0]=choose_point_index
-            elif int(data_result[i][0])==1:       #将第1号分点加在central_points最后
-                data_result[i][0]= len(central_points)
-        #更新中心点
-        central_points[choose_point_index]=add_centers[0]    #将已经被分割的第i号簇变为第0号子簇
-        central_points.append(add_centers[1])                #再加入第1号子簇
-        #更新record
-        j=0
-        for i in range(data_num):
-            if record[i][0]==choose_point_index:              #该数据点属于被分割之前的簇
-                record[i],j=data_result[j],j+1
-    return central_points,record
+            new_centers, new_result = Kmeans(cluster_points, 2)
+            sse_split = sum(new_result[:, 1])
+            sse_non_split = sum(record[j][1] for j in range(data_num) if record[j][0] != i)
+            if sse_split + sse_non_split < min_sse:
+                best_split_idx = i
+                min_sse = sse_split + sse_non_split
+                best_new_centers = new_centers
+                best_new_result = new_result.copy()
+
+        for j in range(len(best_new_result)):
+            if best_new_result[j][0] == 1:
+                best_new_result[j][0] = len(central_points)
+            best_new_result[j][0] = best_split_idx if best_new_result[j][0] == 0 else best_new_result[j][0]
+
+        central_points[best_split_idx] = best_new_centers[0].tolist()
+        central_points.append(best_new_centers[1].tolist())
+        idx = 0
+        for j in range(data_num):
+            if record[j][0] == best_split_idx:
+                record[j] = best_new_result[idx]
+                idx += 1
+
+    return central_points, record
+
+
+def show_cluster_optimized(dataset, central_points, result, save_name, sample_size=10000):
+    """
+    Optimized visualization of clustering results.
+    Randomly samples a subset of points for display to improve performance.
+    """
+    num_clusters = len(central_points)
+    colors = [
+        "red", "green", "blue", "yellow", "orange", "purple",
+        "pink", "gray", "brown", "cyan", "magenta"
+    ]
+
+    # Randomly sample points if dataset is large
+    if len(dataset) > sample_size:
+        sampled_indices = random.sample(range(len(dataset)), sample_size)
+        sampled_dataset = [dataset[i] for i in sampled_indices]
+        sampled_result = [result[i] for i in sampled_indices]
+    else:
+        sampled_dataset = dataset
+        sampled_result = result
+
+    # Scatter plot for sampled data
+    for i, data_point in enumerate(sampled_dataset):
+        cluster_idx = int(sampled_result[i][0])  # Cluster assignment
+        plt.scatter(data_point[0], data_point[1], color=colors[cluster_idx % len(colors)], s=10)
+
+    # Plot centroids
+    for center in central_points:
+        plt.scatter(center[0], center[1], color="black", marker="x", s=100, linewidths=3)
+
+    plt.title("Clustering Result (Optimized)")
+    plt.savefig(f"{save_name}_optimized.png")
+    plt.show()
+
 
 def main():
-    dataset1=load_dataset("data1.csv",',')     #load_dataset()第二个参数为分隔符
-    dataset,k=[[]],0
-    print("data1,6000条数据")
-    dataset=dataset1
-    k=3
+    dataset = load_dataset("box3.csv", ',')  # Adjust path as necessary
+    k = 6  # Number of clusters
+    print("Data loaded successfully.")
+    print(f"Dataset size: {len(dataset)} records.")
+    print(f"Target number of clusters: {k}")
 
-    
+    #print(dataset)
+    #central_points, result = Kmeans(dataset, k)  # Perform KMeans clustering
+    #show_cluster_optimized(dataset, central_points, result, 'final_kmeans')  # Show and save final cluster plot
 
-    print("开始Kmeans聚类")
-    print("数据集大小为",len(dataset))
-    print("目标簇数为",k)
-   # print(dataset)
+    judge = True  # Do not visualize step-by-step for simplicity
+    #showElbow(dataset, 10)  # Display the elbow method plot for optimal k
+    central_points, result = BiKmeans(dataset, k,judge)  # Perform BiKMeans clustering
+    #show_cluster_optimized(dataset, central_points, result, 'final_corrected')  # Show and save final cluster plot
+    show_cluster_optimized(dataset, central_points, result, 'final_corrected')
 
-    flag2=input("请选择是否要进行二分Kmeans的分步执行(0(否)或者1(是))")
-    judge=0
-    if flag2=='0':
-        judge=False
-    elif flag2=='1':
-        judge=True
-    else:
-        print("输入错误")
-        sys.exit(0)
-    showElbow(dataset,10)                                                   #是否显示手肘法折线图？
-    central_points,result=BiKmeans(dataset,k,judge)                          #二分Kmeans中有分步可视化,更改K的值
-    #central_points,result=Kmeans(dataset,3)                                 #正常Kmeans，取点采用了Kmeans++
-    show_cluster(dataset,central_points,result,'final')            #保存最后一张结果图名为'final'
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
