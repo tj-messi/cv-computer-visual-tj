326d551978cc647d1f8b0d51c4c74fd6f59df28b
Junze Zhu <2715978522@qq.com>
2025-02-17 19:28:11 +0800

    Delete 0.跟做项目 directory

--- 0.跟做项目/BAMMFR/修改/论文-A Face Privacy Protection and Self-decryption Method Based on Humanoid Association Mechanism.md
+++
@@ -1,2 +0,0 @@
-论文-A Face Privacy Protection and Self-decryption Method Based on Humanoid Association Mechanism 
-
--- 0.跟做项目/前列腺炎诊断--医疗影像大模型/3.服务器布置/mysite项目更新/mysite-2-7/llm/utils.py
+++
@@ -1,7 +0,0 @@
-# llm/utils.py
-
-def get_hello_world_message():
-    """
-    返回 "Hello, World!" 消息。
-    """
-    return "Hello, World!"
